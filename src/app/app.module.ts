44d4ab6c1a0f64fb3cd4136d73ca6964f5c76f56
vic3214 <victorhornachos@gmail.com>
2022-08-06 10:14:55 +0200

    Estructutra de carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/pages/login/login.component';
+import { RegistroComponent } from './auth/pages/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent,
+    AuthComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
